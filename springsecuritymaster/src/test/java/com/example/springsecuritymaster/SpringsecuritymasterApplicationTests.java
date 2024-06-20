a847646f506738a85ff78deaa7aceb2346cdb46c
yongjung95 <yongjung95@gmail.com>
2024-06-20 22:47:40 +0900

    :white_check_mark: 01. 초기화 과정 이해 - 진행중

---
+++ springsecuritymaster/src/test/java/com/example/springsecuritymaster/SpringsecuritymasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecuritymaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritymasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
