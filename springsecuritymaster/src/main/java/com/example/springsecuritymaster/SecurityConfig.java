98be4e9fb3b271a4c0e610942337c777a3c15cfa
yongjung95 <yongjung95@gmail.com>
2024-06-21 23:45:12 +0900

    :white_check_mark: 01. 초기화 과정 이해

---
+++ springsecuritymaster/src/main/java/com/example/springsecuritymaster/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.springsecuritymaster;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1111")
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.withUsername("user2")
+                .password("{noop}2222")
+                .roles("USER")
+                .build();
+
+        UserDetails user3 = User.withUsername("user3")
+                .password("{noop}3333")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, user2, user3);
+    }
+}
