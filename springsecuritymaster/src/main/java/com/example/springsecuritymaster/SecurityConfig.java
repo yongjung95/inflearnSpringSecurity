bc6e9c2958a7f3cad0e550c4afd7ff2d1de7af94
yongjung95 <yongjung95@gmail.com>
2024-07-04 23:52:03 +0900

    :white_check_mark: 05. 세션 관리 : 세션 생성 정책 - sessionManagement().sessionCreationPolicy()

--- springsecuritymaster/src/main/java/com/example/springsecuritymaster/SecurityConfig.java
+++ springsecuritymaster/src/main/java/com/example/springsecuritymaster/SecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -25,7 +25,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .sessionManagement(session -> session
-                                .sessionFixation(SessionManagementConfigurer.SessionFixationConfigurer::changeSessionId)
+                                .sessionCreationPolicy(SessionCreationPolicy.NEVER)
                 )
         ;
 
